4cb85020da799e3dcd55184189eb08d1f1b0a622
Xandreyyy <alejandroesteves2005@gmail.com>
2023-12-06 17:21:16 -0300

    creating a UI Python

---
+++ USANDO_VERSIONAMENTO/simular_projeto2/UI.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+# gera o frame
+window = tk.Tk()
+# informamos largura x altura do frame
+window.geometry('300x300')
+# título do frame
+window.title('Meu app Py')
+# gera um texto
+label = tk.Label(text = 'Oi mãe')
+# ???
+label.grid(column = 2, row = 4)
+# define a ancoragem no centro
+label.pack(anchor = 'center')
+
+# usado para mostrar na tela
+window.mainloop()
