b2bfc2bfd4f104bfd46a0cf87ebdcbe376ecdf90
Xandreyyy <alejandroesteves2005@gmail.com>
2023-12-12 15:57:41 -0300

    1st commit -> Person class

---
+++ USANDO_VERSIONAMENTO/age_calculator/Person.py
@@ -0,0 +1,25 @@
+from datetime import datetime as dt
+
+class Person:
+    def __init__(self, name: str, b_date: str) -> None:
+        self.__name = name
+        self.__birth_date = b_date[6:]
+
+    @property
+    def name(self):
+        return self.__name 
+
+    @name.setter      
+    def name(self, name: str) -> None:
+        self.__name = name
+    
+    @property
+    def b_date(self):
+        return int(self.__birth_date)
+
+    @b_date.setter
+    def b_date(self, b_date: str) -> None:
+        self.__birth_date = b_date
+
+    def get_years(self):
+        return dt.today().year - self.b_date
