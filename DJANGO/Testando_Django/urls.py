bdf9497edd3d2c32830d309eff95e0c0194a3bbd
Xandreyyy <alejandroesteves2005@gmail.com>
2024-01-22 17:22:36 -0300

    creating a template to Testando_Django

--- DJANGO/Testando_Django/urls.py
+++ DJANGO/Testando_Django/urls.py
@@ -15,18 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("[HOME] Funcionando!")
-
-def blog(request):
-    print("[BLOG] Executando outras ações...")
-    return HttpResponse("[BLOG] Funcionando!")
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path("blog/", blog)
+    path('', include("home.urls")),
+    path('blog/', include("blog.urls")),
+    path('exemplo/', include("blog.urls"))
 ]
--- DJANGO/blog/views.py
+++ DJANGO/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
+def blog(request):
+    print("[BLOG] Executando outras ações...")
+    return HttpResponse("[BLOG] Funcionando!")
+
+def exemplo(request):
+    return HttpResponse("[BLOG -> EZEMPLO] Funcionando!")
