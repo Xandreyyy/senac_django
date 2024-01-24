2b03e5a4f88672e010f7092d2c9a1f39c2a844a9
Xandreyyy <alejandroesteves2005@gmail.com>
2024-01-24 17:05:30 -0300

    setting up for each post define a url

--- DJANGO/blog/views.py
+++ DJANGO/blog/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+from blog.data import posts
 
 def blog(request):
     print("[BLOG] Executando outras ações...")
-    context = {
-        "title": "BLOG",
-        "text": "Estamos no BLOG"
-    }
-    return render(request, "blog/index.html", context)
+    return render(request,
+                  "blog/index.html",
+                 {
+                  "title": "Blog",
+                  "posts": posts   
+                 }
+    )
+
+def post(request, id):
+    print("[POST] Executando outras ações...")
+    return render(request,
+                  "blog/index.html",
+                 {
+                  "title": "Post",
+                  "posts": posts   
+                 }
+    )
 
 def exemplo(request):
     context = {
