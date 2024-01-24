223944a3f8034bd40487a00960de73cc8d1da785
Xandreyyy <alejandroesteves2005@gmail.com>
2024-01-24 16:16:49 -0300

    placing content dynamically

--- DJANGO/blog/urls.py
+++ DJANGO/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.blog, name = "blog"),
+    path('', views.blog, name = "home"),
     path('exemplo/', views.exemplo, name = "exemplo")
 ]
